1cdc5130fe4c4194dbb4222830fca3ee5ca006f3
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2024-11-21 16:26:08 +0100

    Create boilerplate mains for exercises

---
+++ src/amount/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+  std::cout << "0x00 Amount" << std::endl; 
+  return 0;
+}
---
+++ src/catch-and-run/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+  std::cout << "0x10 Catch & Run" << std::endl; 
+  return 0;
+}
